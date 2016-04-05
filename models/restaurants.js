e23f0d5ff5045349f08151d6825e0388cebe2da4
Vianney-VICTORIA <vianneyvictoria@gmail.com>
2016-04-05 19:31:07 +0200

    feat resto schema

--- models/restaurants.js
+++ models/restaurants.js
@@ -1,7 +1,21 @@
 const mongoose = require('mongoose');
 
 const RestaurantsSchema = new mongoose.Schema({
-  // Complete schema...
+      _id: { type : Schema.ObjectId },
+      adress: [{
+         coord: [],
+         street: { type : String, default : '', trim : true },
+         zipcode: { type : String, default : '', trim : true },
+      }],
+      borought: { type : String, default : '', trim : true },
+      cuisine: { type : String, default : '', trim : true },
+      comments: [{
+         date: { type : Date, default : Date.now },
+         grade: { type : String, default : '', trim : true },
+         score: { type : Number, default : '' }
+      }],
+      name: { type : String, default : '', trim : true },
+      restaurant_id: { type : String, default : '', trim : true },
 });
 
 module.exports = mongoose.model('Restaurant', RestaurantsSchema);
