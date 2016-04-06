7e02698b1906ffdc60cde0fbbc90f21d4d7c9984
Maxime Franc <maxime.franc1@gmail.com>
2016-04-07 00:18:45 +0200

    generate list of restaurants on home

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var Restaurants = require('../models/restaurants');
+
+// Restaurants.methods.getGrade = () => {
+// 	return Restaurants.count{(grades.score)} / Restaurants.count{(grades)};
+// } sort(grades.score) // POUR CLASSER LE FIND
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  Restaurants.find({}).limit(10).exec().then((datas) => {
+  	console.log(datas);
+  	res.render('index', {datas});
+  });
 });
 
 module.exports = router;
